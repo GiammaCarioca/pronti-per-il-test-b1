8cb939bf46e4846e0f273722b0ab362a1f0c7632
Thiago Giammattey <giammattey@gmail.com>
2020-11-14 18:32:16 -0300

    move index.html

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
     require("tailwindcss"),
     require("autoprefixer"),
     require("@fullhuman/postcss-purgecss")({
-      content: ["./public/*.html"],
+      content: ["./index.html", "./public/*.html"],
       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     }),
   ],
