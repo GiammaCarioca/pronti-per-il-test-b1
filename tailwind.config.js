8050feda0a134b4782f85e874691ed7bdd7c81e1
Thiago Giammattey <giammattey@gmail.com>
2020-11-17 10:08:33 -0300

    better styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,38 +14,17 @@ module.exports = {
     require("@tailwindcss/custom-forms"),
     function ({ e, addUtilities }) {
       const newUtilities = {
-        ".error": {
-          backgroundColor: "#f56565",
-          borderRadius: "4px",
-          padding: "0.5rem",
-          font: "inherit",
-          color: "#f7fafc",
-          lineHeight: "1.5em",
-          boxShadow:
-            "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
-        },
-        ".right": {
-          backgroundColor: "#48bb78",
-          borderRadius: "4px",
-          padding: "0.5rem",
-          font: "inherit",
-          color: "#f7fafc",
-          lineHeight: "1.5em",
-          boxShadow:
-            "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
-        },
-        ".select": {
-          backgroundColor: "rgba(200, 200, 200, 0.4)",
-          border: "1px solid transparent",
-          borderRadius: "4px",
-          display: "inline-block",
-          font: "inherit",
-          lineHeight: "1.5em",
-
-          /* reset */
-          margin: "0.1em",
-          boxSizing: "border-box",
-        },
+        // ".select": {
+        //   backgroundColor: "rgba(200, 200, 200, 0.4)",
+        //   border: "1px solid transparent",
+        //   borderRadius: "4px",
+        //   display: "inline-block",
+        //   font: "inherit",
+        //   lineHeight: "1.5em",
+        //   /* reset */
+        //   margin: "0.1em",
+        //   boxSizing: "border-box",
+        // },
       };
 
       addUtilities(newUtilities);
