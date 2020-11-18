159a4523ad82dfce4e8c494a30466f3d02396911
Thiago Giammattey <giammattey@gmail.com>
2020-11-18 15:26:48 -0300

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,20 @@ module.exports = {
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: { backgroundColor: ["checked"] },
   plugins: [
     require("@tailwindcss/custom-forms"),
     function ({ e, addUtilities }) {
       const newUtilities = {
+        ".custom": {
+          cursor: "pointer",
+          borderRadius: "50%",
+          padding: "0.6rem",
+          fontFamily: "monospace",
+          textTransform: "lowercase",
+          border: "1px solid transparent",
+          backgroundColor: "rgba(107, 184, 218, 0.4)",
+        },
         // ".select": {
         //   backgroundColor: "rgba(200, 200, 200, 0.4)",
         //   border: "1px solid transparent",
